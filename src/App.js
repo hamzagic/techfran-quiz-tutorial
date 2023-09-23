215cb93e8cb96485a5cdbd1ad4d4b98e44e02b89
Francis <francishamzagic@gmail.com>
2023-09-23 13:40:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div>
+        <h1>Super Quiz</h1>
+      </div>
+      <div>
+        <h3>First Question of 4</h3>
+      </div>
+      <div className='options-container'>
+        <div className='option-item'>
+          <input type="checkbox" id="1" name="answer-1" checked />
+          <label for="answer-1">Answer 1</label>
+        </div>
+
+        <div className='option-item'>
+          <input type="checkbox" id="2" name="answer-2" />
+          <label for="answer-2">Answer 2</label>
+        </div>
+
+        <div className='option-item'>
+          <input type="checkbox" id="3" name="answer-3" />
+          <label for="answer-3">Answer 3</label>
+        </div>
+
+        <div className='option-item'>
+          <input type="checkbox" id="4" name="answer-4" />
+          <label for="answer-4">Answer 4</label>
+        </div>
+      </div>
+
+      <div className='next-button'>
+        <button>Next</button>
+      </div>
     </div>
   );
 }
